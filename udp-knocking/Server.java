afdbc56091466f8a416d99bf8fde3b1529944662
jakubpartyka <j.partyka20@gmail.com>
2021-01-23 17:07:00 +0100

    Datagram socket opened, unnecessary code commented out

--- udp-knocking/Server.java
+++ udp-knocking/Server.java
@@ -2,12 +2,17 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        // config
         int [] sequence = new int[args.length];
-        HashSet<Integer> ports = new HashSet<Integer>();
+        HashSet<Integer> ports = new HashSet<>();
+
+        // connection objects
+        ArrayList<DatagramSocket> sockets = new ArrayList<>();
 
         // set sequence
         for (int i = 0; i < args.length; i++)
@@ -18,33 +23,38 @@ public class Server {
             ports.add(Integer.parseInt(arg));
 
 
-
-        DatagramSocket socket = new DatagramSocket(4445);
-
-        boolean running;
-        byte[] buf = new byte[256];
-
-        running = true;
-
-        while (running) {
-            DatagramPacket packet
-                    = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
-            packet = new DatagramPacket(buf, buf.length, address, port);
-            String received = new String(packet.getData(), 0, packet.getLength());
-
-            System.out.println("received: " + received);
-
-            if (received.equals("end")) {
-                running = false;
-                continue;
-            }
-            socket.send(packet);
+        // OPEN DATAGRAM PORTS
+        for (Integer port : ports) {
+            DatagramSocket socket = new DatagramSocket(port);
+            sockets.add(socket);
         }
-        socket.close();
+
+
+
+//        boolean running;
+//        byte[] buf = new byte[256];
+//
+//        running = true;
+//
+//        while (running) {
+//            DatagramPacket packet
+//                    = new DatagramPacket(buf, buf.length);
+//            socket.receive(packet);
+//
+//            InetAddress address = packet.getAddress();
+//            int port = packet.getPort();
+//            packet = new DatagramPacket(buf, buf.length, address, port);
+//            String received = new String(packet.getData(), 0, packet.getLength());
+//
+//            System.out.println("received: " + received);
+//
+//            if (received.equals("end")) {
+//                running = false;
+//                continue;
+//            }
+//            socket.send(packet);
+//        }
+//        socket.close();
 
     }
 }
