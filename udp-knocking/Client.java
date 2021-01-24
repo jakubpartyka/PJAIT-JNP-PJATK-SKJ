e23604dcfb68aeb236606e134c4cefc47c4b9f0d
jakubpartyka <j.partyka20@gmail.com>
2021-01-24 18:11:32 +0100

    knockOnPort method added ; knock sequence implemented

--- udp-knocking/Client.java
+++ udp-knocking/Client.java
@@ -2,27 +2,39 @@ import java.io.IOException;
 import java.net.*;
 
 public class Client {
+    // CONFIGURATION
+    private static final String KNOCK_MESSAGE = "KNOCK KNOCK";
+
+    // CONNECTION OBJECTS
     private static DatagramSocket socket;
     private static InetAddress address;
     private static byte[] buf;
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         socket = new DatagramSocket();
         address = InetAddress.getByName("localhost");
 
-        String response = sendEcho("KNOCK KNOCK");
-        System.out.println(response);
+        knockOnPort(20202);
+        Thread.sleep(100);
+        knockOnPort(20203);
+        Thread.sleep(100);
+        knockOnPort(40500);
+        Thread.sleep(100);
+        knockOnPort(50002);
+
     }
 
-    public static String sendEcho(String msg) throws IOException {
-        buf = msg.getBytes();
-        DatagramPacket packet
-                = new DatagramPacket(buf, buf.length, address, 20202);
+    public static void knockOnPort(int port) throws IOException {
+        buf = KNOCK_MESSAGE.getBytes();
+
+        // create and send packet
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
         socket.send(packet);
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        String received = new String(
-                packet.getData(), 0, packet.getLength());
-        return received;
+
+//        packet = new DatagramPacket(buf, buf.length);
+//        socket.receive(packet);
+//        String received = new String(
+//                packet.getData(), 0, packet.getLength());
     }
 }
